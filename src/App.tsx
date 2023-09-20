8a439d3b8e9a275b4a3dd93c9f7808b0a3829ad5
Rini <Reneortega2468@gmail.com>
2023-09-20 14:35:49 -0400

    Added sentry as a test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import store from './redux/store'
 import * as Sentry from 'sentry-expo'
 
 Sentry.init({
-    dsn: 'https://d67b5dfe88920e6a3f9d8f4b58cecd39@o4505913662046208.ingest.sentry.io/4505913677053952',
+    dsn: 'YOUR DSN HERE',
     enableInExpoDevelopment: true,
     debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 })
